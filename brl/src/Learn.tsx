91cecc88944d0699bc010e6a8d584e5dd2d16963
JasonSoo123 <soojason4@gmail.com>
2024-10-05 19:13:19 -0700

    bg

--- brl/src/Learn.tsx
+++ brl/src/Learn.tsx
@@ -1,5 +1,17 @@
 const Learn: React.FC = () => {
-    return <div>Learn Page</div>;
+    const bgStyle = {
+        backgroundImage: 'url("images/jh.jpg")',
+        backgroundSize: 'cover', // Adjusts the size of the image to cover the entire div
+        backgroundPosition: 'center', // Centers the image
+        height: '100vh', // Full viewport height
+        width: '100%', // Full width
+      };
+    return <div style={bgStyle}>
+        
+
+
+
+    </div>;
   };
   
 export default Learn;
