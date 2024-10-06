5f1b8ccdd723f615ce077cadfcffeb75a18a3135
Andrew La Grange <ala284@sfu.ca>
2024-10-05 19:20:49 -0700

    Made a drop down

--- brl/src/Dictionary.tsx
+++ brl/src/Dictionary.tsx
@@ -1,5 +1,24 @@
+import React, { useState } from 'react';
+
 const Dictionary: React.FC = () => {
-    return <div>Dictionary Page</div>;
+    const [isOpen, setIsOpen] = useState(false);
+
+    const onClick = () => {
+        setIsOpen((prev) => !prev);
+    }
+
+    return <div className="dropdown">
+            <button className="dropdown-button" onClick={onClick}>
+                A - names...
+                </button>
+                {isOpen && (
+                    <div className="dropdown-content">
+                        <h3>Dropdown Content</h3>
+                        <p>This dropdown appears on button click.</p>
+                        <p>More information can be added here.</p>
+                    </div>
+                )}
+  </div>
   };
   
   export default Dictionary;
