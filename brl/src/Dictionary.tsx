a505a9d29c0057c1744cbd5bf43e031736832168
Andrew La Grange <ala284@sfu.ca>
2024-10-05 21:07:09 -0700

    Button format good

--- brl/src/Dictionary.tsx
+++ brl/src/Dictionary.tsx
@@ -10,11 +10,12 @@ const Dictionary: React.FC = () => {
 
     const buttonPlacement: React.CSSProperties = {
         display: "flex",
-        justifyContent: "center",
+        //justifyContent: "center",
         alignItems: "center", 
         flexDirection: "column",
         height: '100vh',
         width: '100%',
+        paddingTop: '100px',
     };
 
     const bgStyle = {
@@ -33,7 +34,11 @@ const Dictionary: React.FC = () => {
         borderRadius: "5px", // Rounded corners
         cursor: "pointer", // Pointer cursor on hover
         width: '1000px', // Width for the button
-        height: '50px', // Height for the button
+        height: '100px', // Height for the button
+        border: '2px solid #000000',
+        fontFamily: 'Roboto, sans-serif',
+        fontSize: '40px',
+        textAlign: 'left',
       };
 
     return (
@@ -42,17 +47,22 @@ const Dictionary: React.FC = () => {
         <div className="">
         <header>
             <nav>
-            <ul>
-                <li>
-                <Link to="/" activeClassName="active">Home</Link>
-                </li>
-                <li>
-                <Link to="/learn" activeClassName="active">Learn</Link>
-                </li>
-                <li>
-                <Link to="/dictionary" activeClassName="active">Dictionary</Link>
-                </li>
-            </ul>
+                {/* Logo on the left */}
+                <Link to="/">
+                        <img src="/images/default-logo.png" alt="Logo" className="logo-img" />
+                </Link>
+                {/* Navigation links in the center */}
+                <ul>
+                    <li>
+                    <Link to="/" activeClassName="active">Home</Link>
+                    </li>
+                    <li>
+                    <Link to="/learn" activeClassName="active">Learn</Link>
+                    </li>
+                    <li>
+                    <Link to="/dictionary" activeClassName="active">Dictionary</Link>
+                    </li>
+                </ul>
             </nav>
         </header>
         </div>
@@ -63,17 +73,9 @@ const Dictionary: React.FC = () => {
             <button 
                 className="dropdown-button" 
                 onClick={onClick} 
-                // style={{
-                // padding: "10px 20px", // Padding for the button
-                // backgroundColor: "#00a994", // Button background color
-                // color: "white", // Text color
-                // border: "none", // No border
-                // borderRadius: "5px", // Rounded corners
-                // cursor: "pointer", // Pointer cursor on hover
-                // }}
                 style={buttonStyle}
-        >
-                A - names... {/* Text inside the button */}
+        >       {/* Text inside the button */}
+                A - names... 
             </button>
         {isOpen && (
             <div className="dropdown-content">
