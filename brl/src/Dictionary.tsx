ce9beedc36731484c9f1bb37f8e0dec44494bc75
Andrew La Grange <ala284@sfu.ca>
2024-10-05 20:43:36 -0700

    Navigation added to Dictionary

--- brl/src/Dictionary.tsx
+++ brl/src/Dictionary.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 
 const Dictionary: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -35,23 +36,45 @@ const Dictionary: React.FC = () => {
         height: '50px', // Height for the button
       };
 
-    return <div className="dropdown" style={bgStyle}>
-        <div style={buttonPlacement}>
-        <button 
-            className="dropdown-button" 
-            onClick={onClick} 
-            // style={{
-            // padding: "10px 20px", // Padding for the button
-            // backgroundColor: "#00a994", // Button background color
-            // color: "white", // Text color
-            // border: "none", // No border
-            // borderRadius: "5px", // Rounded corners
-            // cursor: "pointer", // Pointer cursor on hover
-            // }}
-            style={buttonStyle}
+    return (
+
+        <>
+        <div className="">
+        <header>
+            <nav>
+            <ul>
+                <li>
+                <Link to="/" activeClassName="active">Home</Link>
+                </li>
+                <li>
+                <Link to="/learn" activeClassName="active">Learn</Link>
+                </li>
+                <li>
+                <Link to="/dictionary" activeClassName="active">Dictionary</Link>
+                </li>
+            </ul>
+            </nav>
+        </header>
+        </div>
+    
+
+        <div className="dropdown" style={bgStyle}>
+            <div style={buttonPlacement}>
+            <button 
+                className="dropdown-button" 
+                onClick={onClick} 
+                // style={{
+                // padding: "10px 20px", // Padding for the button
+                // backgroundColor: "#00a994", // Button background color
+                // color: "white", // Text color
+                // border: "none", // No border
+                // borderRadius: "5px", // Rounded corners
+                // cursor: "pointer", // Pointer cursor on hover
+                // }}
+                style={buttonStyle}
         >
-            A - names... {/* Text inside the button */}
-        </button>
+                A - names... {/* Text inside the button */}
+            </button>
         {isOpen && (
             <div className="dropdown-content">
             <h3>Dropdown Content</h3>
@@ -61,6 +84,8 @@ const Dictionary: React.FC = () => {
         )}
         </div>
     </div>
+    </>
+    );
   };
   
   export default Dictionary;
