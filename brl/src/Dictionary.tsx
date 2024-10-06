c8b107ab5ad686c2aa85f20951ce6d175be9878a
Andrew La Grange <ala284@sfu.ca>
2024-10-06 05:04:24 -0700

    Fixed it

--- brl/src/Dictionary.tsx
+++ brl/src/Dictionary.tsx
@@ -3,80 +3,59 @@ import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import DropdownButton from './DropdownButton.tsx';
 import data from '../public/termDictionary.json'; // Import your JSON file
-
+import "./Dictionary.css"
 import "./App.css"
 
 const Dictionary: React.FC = () => {
-    const buttonPlacement: React.CSSProperties = {
-        display: "flex",
-        alignItems: "center", 
-        flexDirection: "column",
-        height: '100vh',
-        width: '100%',
-        paddingTop: '100px',
-    };
-
-    const bgStyle: React.CSSProperties = {
-        flex: 1,
-        backgroundColor: "#ddc644"
-    };
-
-    return (
-
-        <>
-        <div style={bgStyle}>
-        <header>
-            <nav>
-                {/* Logo on the left */}
-                <Link to="/">
-                        <img src="/images/default-logo.png" alt="Logo" className="logo-img" />
-                </Link>
-                {/* Navigation links in the center */}
-                <ul>
-                    <li>
-                    <NavLink to="/" activeClassName="active">Home</NavLink>
-                    </li>
-                    <li>
-                    <NavLink to="/learn" activeClassName="active">Learn</NavLink>
-                    </li>
-                    <li>
-                    <NavLink to="/dictionary" activeClassName="active">Dictionary</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-        
     
 
-        <div className="dropdown" style={bgStyle}>
-        <div style={buttonPlacement}>
-                {/* Dynamically Generate Dropdown Buttons from JSON */}
-                {data.map((item, index) => (
-                    <DropdownButton
-                        key={index}
-                        buttonText={item.term}
-                        dropdownContent={
-                            <div>
-                                <p><strong>Definition:</strong> {item.definition}</p>
-                                <p><strong>Example:</strong> {item.examples}</p>
-                            </div>
-                        }
-                    />
-                ))}
+    return (
+        <>
+            <div className="bg-style">
+                <header>
+                    <nav>
+                        {/* Logo on the left */}
+                        <Link to="/">
+                            <img src="/images/default-logo.png" alt="Logo" className="logo-img" />
+                        </Link>
+                        {/* Navigation links in the center */}
+                        <ul>
+                            <li>
+                                <NavLink to="/" activeClassName="active">Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/learn" activeClassName="active">Learn</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dictionary" activeClassName="active">Dictionary</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+    
+                <div className="button-placement">
+                    {/* Dynamically Generate Dropdown Buttons from JSON */}
+                    {data.map((item, index) => (
+                        <DropdownButton
+                            key={index}
+                            buttonText={item.term}
+                            dropdownContent={
+                                <div>
+                                    <p><strong>Definition:</strong> {item.definition}</p>
+                                    <p><strong>Example:</strong> {item.examples}</p>
+                                </div>
+                            }
+                        />
+                    ))}
+                </div>
+    
+                <footer className="footer">
+                    <p>© 2024 BrainRot Lingo. All rights reserved.</p>
+                </footer>
             </div>
-
-
-      {/* Add more DropdownButtons as needed */}
-    {/*</div>*/}
-
-        </div>
-        <footer className="footer">
-          <p>© 2024 BrainRot Lingo. All rights reserved.</p>
-        </footer>
-    {/*</></div>*/}
-    </div>
-    </>
+        </>
     );
+    
   };
   
   export default Dictionary;
