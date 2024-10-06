5e6c875ddf9a89db08973a2aa203f9c7fb2daade
Andrew La Grange <ala284@sfu.ca>
2024-10-05 20:05:34 -0700

    Button in middle with dropdown

--- brl/src/Dictionary.tsx
+++ brl/src/Dictionary.tsx
@@ -5,20 +5,50 @@ const Dictionary: React.FC = () => {
 
     const onClick = () => {
         setIsOpen((prev) => !prev);
-    }
+    };
 
-    return <div className="dropdown">
-            <button className="dropdown-button" onClick={onClick}>
-                A - names...
-                </button>
-                {isOpen && (
-                    <div className="dropdown-content">
-                        <h3>Dropdown Content</h3>
-                        <p>This dropdown appears on button click.</p>
-                        <p>More information can be added here.</p>
-                    </div>
-                )}
-  </div>
+    const buttonPlacement: React.CSSProperties = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center", 
+        flexDirection: "column",
+        height: '100vh',
+        width: '100%',
+    };
+
+    const bgStyle = {
+        backgroundImage: 'url("images/jh.jpg")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        width: '100%',
+    };
+
+    return <div className="dropdown" style={bgStyle}>
+        <div style={buttonPlacement}>
+        <button 
+            className="dropdown-button" 
+            onClick={onClick} 
+            style={{
+            padding: "10px 20px", // Padding for the button
+            backgroundColor: "#4CAF50", // Button background color
+            color: "white", // Text color
+            border: "none", // No border
+            borderRadius: "5px", // Rounded corners
+            cursor: "pointer", // Pointer cursor on hover
+            }}
+        >
+            A - names... {/* Text inside the button */}
+        </button>
+        {isOpen && (
+            <div className="dropdown-content">
+            <h3>Dropdown Content</h3>
+            <p>This dropdown appears on button click.</p>
+            <p>More information can be added here.</p>
+            </div>
+        )}
+        </div>
+    </div>
   };
   
   export default Dictionary;
